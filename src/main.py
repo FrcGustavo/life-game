53b5a70a7c9b158738740b6e30fb80337158ffd7
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-18 19:00:36 -0500

    👷[Add] controls with mouse and keyboard

--- src/main.py
+++ src/main.py
@@ -38,6 +38,8 @@ gameState[22, 23] = 1
 gameState[21, 23] = 1
 gameState[20, 23] = 1
 
+# Control execution control 
+pauseExect = False
 
 while True:
 
@@ -46,26 +48,43 @@ while True:
 	screen.fill(bg)
 	time.sleep(0.1)
 
+	# Register events to keyboard and mouse
+	ev = pygame.event.get()
+
+	for event in ev:
+		# Detect if a key is pressed 
+		if event.type == pygame.KEYDOWN:
+			pauseExect = not pauseExect
+
+		# Detect if a mouse is pressed
+		mouseClick = pygame.mouse.get_pressed()
+
+		if sum(mouseClick) > 0:
+			posX, posY = pygame.mouse.get_pos()
+			celX, celY = int(np.floor(posX / dimCW)), int(np.floor(posY / dimCH))
+			newGameState[celX, celY] = not mouseClick[2]
+
 	for y in range(0, nxC):
 		for x in range(0, nyC):
 
-			# Calc the number of close neighbors.
-			n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
-								gameState[(x)			% nxC, (y - 1) % nyC] + \
-								gameState[(x + 1) % nxC, (y - 1) % nyC] + \
-								gameState[(x - 1) % nxC, (y)		 % nyC] + \
-								gameState[(x + 1) % nxC, (y)		 % nyC] + \
-								gameState[(x - 1) % nxC, (y + 1) % nyC] + \
-								gameState[(x) 		% nxC, (y + 1) % nyC] + \
-								gameState[(x + 1) % nxC, (y + 1) % nyC]
+			if not pauseExect:
+				# Calc the number of close neighbors.
+				n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+									gameState[(x)			% nxC, (y - 1) % nyC] + \
+									gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+									gameState[(x - 1) % nxC, (y)		 % nyC] + \
+									gameState[(x + 1) % nxC, (y)		 % nyC] + \
+									gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+									gameState[(x) 		% nxC, (y + 1) % nyC] + \
+									gameState[(x + 1) % nxC, (y + 1) % nyC]
 
-			# Rule #1: One cellular dead with 3 exact lifiving neighbors, "revives".
-			if gameState[x, y] == 0 and n_neigh == 3:
-				newGameState[x, y] = 1
+				# Rule #1: One cellular dead with 3 exact lifiving neighbors, "revives".
+				if gameState[x, y] == 0 and n_neigh == 3:
+					newGameState[x, y] = 1
 
-			# Rule #2: One cellular living with less than 2 or more than 3 living neighbors, "dead".
-			elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
-				newGameState[x, y] = 0 
+				# Rule #2: One cellular living with less than 2 or more than 3 living neighbors, "dead".
+				elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+					newGameState[x, y] = 0 
 
 			# Create the polygon of each cell to draw.
 			poly = [
